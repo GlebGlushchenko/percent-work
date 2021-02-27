bbee0a7f7b977ee45006105070a1452f53c10c58
GlebGlushchenko <Darkvee1n@yandex.ru>
2021-02-28 00:46:34 +0300

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const number = document.getElementById('number');
+const percent = document.getElementById('percent');
+const btn = document.getElementById('btn');
+const resultElement = document.getElementById('result');
+
+const test = (number, percent) => {
+  const finalPercent = (number * percent) / 100;
+  resultElement.innerHTML = finalPercent;
+};
+
+btn.addEventListener('click', () => {
+  test(number.value, percent.value);
+});
